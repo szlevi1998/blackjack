47619d124f1d8a226c59492fc321ab9861f04399
sz.levi1998@gmail.com <sz.levi1998@gmail.com>
2020-05-17 15:00:43 +0200

    Images added

--- src/main/java/main/MyApplication.java
+++ src/main/java/main/MyApplication.java
@@ -7,14 +7,10 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class MyApplication extends Application {
-
-
     @Override
-
     public void start(Stage primaryStage) throws Exception{
     Parent root = FXMLLoader.load(getClass().getResource("/fxml/launch.fxml"));
     primaryStage.setTitle("Blackjack");
-    primaryStage.setResizable(false);
     primaryStage.setScene(new Scene(root));
     primaryStage.show();
     }
